32b1debc709e1f4d0c6c1934e5a1230c3e02834f
uto8 <70425214+uto8@users.noreply.github.com>
2022-09-27 23:24:52 +0900

    ログインページ追加

---
+++ frontend/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Welcome from './views/Welcome.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: '/', component: Home
+    },
+    {
+      path: '/welcome', component: Welcome
+    }
+  ]
+});
